40d0128433e8e6bdaa2b6bb95268ac24f6f1e80b
Kerim Njuhovic <kerimnjuhovic@gmail.com>
2023-04-03 13:58:40 +0200

    project setup

---
+++ ToDoList/ToDoList/View/ToDoListApp.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Kerim Njuhovic on 3. 4. 2023..
+//
+
+import SwiftUI
+
+/**
+ 
+ MVVM Architecture
+ 
+ Model - data point
+ View - user interface
+ ViewModel - manages model for the view
+ 
+ */
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
