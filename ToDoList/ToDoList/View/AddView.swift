eae915ace95db5cac74422659a4f165c08f1c929
Kerim Njuhovic <kerimnjuhovic@gmail.com>
2023-04-03 18:05:53 +0200

    code styling for AddView.swift

--- ToDoList/ToDoList/View/AddView.swift
+++ ToDoList/ToDoList/View/AddView.swift
@@ -13,33 +13,59 @@ struct AddView: View {
     
     var body: some View {
         ScrollView {
-            TextField(text: $textFieldText) {
-                Text("Enter your string here")
-            }
-            .frame(height: 50)
-            .background(Color.gray.opacity(0.2))
-            .cornerRadius(10)
-            .padding(.horizontal)
-            Button(action: {
-                
-            }) {
-                Text("Save")
-                    .frame(height: 50)
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-            }
+            textfield
+            saveButton
         }
         .navigationTitle("Add ➕")
     }
 }
 
+// MARK: - Subviews -
+
+fileprivate extension AddView {
+    
+    var textfield: some View {
+        TextField(text: $textFieldText) {
+            Text("Enter your string here")
+        }
+        .roundStyle(background: Color.gray.opacity(0.2))
+    }
+    
+    var saveButton: some View {
+        Button(action: {
+            
+        }) {
+            Text("Save".capitalized)
+                .roundStyle(background: Color.blue)
+                .foregroundColor(.white)
+        }
+    }
+    
+}
+
+// MARK: - Screen styling -
+
+fileprivate extension View {
+    
+    func roundStyle(background: Color) -> some View {
+        self
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .background(background)
+            .cornerRadius(10)
+            .padding(.horizontal)
+    }
+    
+}
+
+// MARK: - Preview -
+
 struct AddView_Previews: PreviewProvider {
+    
     static var previews: some View {
         NavigationView {
             AddView()
         }
     }
+    
 }
